55598ef87b2dcdc92b66f407483c8322a44fbd60
kailani <kailani4@icloud.com>
2026-08-26 12:04:07 -0700

    merge issue

--- scratch.R
+++ scratch.R
@@ -6,79 +6,139 @@ q3_data <- read_csv("Q3.csv")
 mpr_data <- read_csv("MPR.csv")
 
 
-
 # Q1 code ----------------------------------------------------------------
 
-
 # ions <- c(`NH4-N`, `Ca`, `Mg`, `NO3-N`, `K`)
-years <- 1988:1994
-year(q1_data$Sample_Date)
+window_start <- seq(ymd("1988-01-01"), ymd("1994-12-31"), by = "9 weeks")
+
+clean_q1 <- q1_data |>
+  mutate(Date = Sample_Date) |>
+  select(Date, `NH4-N`, Ca, Mg, `NO3-N`, K) |>
+  filter(Date >= "1988-01-01" & Date <= "1994-12-31")
 
 
-clean_q1 <- q1_data |> 
-  mutate(Sample_Date = year(Sample_Date)) |> 
-  filter(Sample_Date >= 1988 & Sample_Date <= 1994) |> 
-  select(Sample_Date, `NH4-N`, Ca, Mg, `NO3-N`, K) |> 
+K <- NA
+Mg <- NA
+Ca <- NA
+`NH4-N` <- NA
+`NO3-N` <- NA
+
+
+q1_smoothed <- tibble(
+  window_start,
+  K,
+  Mg,
+  Ca,
+  `NH4-N`,
+  `NO3-N`
+)
+
+
+for (i in 1:nrow(q1_smoothed)) {
+  start_date <- q1_smoothed$window_start[i]
+  end_date <- q1_smoothed$window_start[i] + weeks(9)
+
+  k_ranges <- clean_q1$K[
+    clean_q1$Date >= start_date &
+      clean_q1$Date < end_date
+  ]
+
+  mg_ranges <- clean_q1$Mg[
+    clean_q1$Date >= start_date &
+      clean_q1$Date < end_date
+  ]
+
+  ca_ranges <- clean_q1$Ca[
+    clean_q1$Date >= start_date &
+      clean_q1$Date < end_date
+  ]
+
+  nh4n_ranges <- clean_q1$`NH4-N`[
+    clean_q1$Date >= start_date &
+      clean_q1$Date < end_date
+  ]
+
+  no3n_ranges <- clean_q1$`NO3-N`[
+    clean_q1$Date >= start_date &
+      clean_q1$Date < end_date
+  ]
+
+  q1_smoothed$K[i] <- mean(k_ranges, na.rm = TRUE)
+  q1_smoothed$Mg[i] <- mean(mg_ranges, na.rm = TRUE)
+  q1_smoothed$Ca[i] <- mean(ca_ranges, na.rm = TRUE)
+  q1_smoothed$`NH4-N`[i] <- mean(nh4n_ranges, na.rm = TRUE)
+  q1_smoothed$`NO3-N`[i] <- mean(no3n_ranges, na.rm = TRUE)
+}
+q1_smoothed
+
+
+q1_smoothed |>
   pivot_longer(
-    cols = !Sample_Date, 
-    names_to = "Ions", 
-    values_to = "Units"
+    cols = !window_start,
+    names_to = "Ions",
+    values_to = "Concentration"
+  ) |>
+  ggplot(
+    q1_smoothed,
+    mapping = aes(
+      x = window_start,
+      y = log(Concentration)
+    )
+  ) +
+  geom_line() +
+  facet_wrap(~Ions) +
+  labs(
+    title = "Concentrations of Various Ions",
+    x = "Year",
+    y = "Log Transformation of Conc."
   )
-
-
-ggplot(
-  clean_q1, 
-  mapping = aes(
-    x = Sample_Date, 
-    y = log(Units)
-  )
-) + 
-  geom_point() +
-  facet_wrap(~Ions)
-
-
 
 
 # Q2 code ----------------------------------------------------------------
 
-
-clean_q2 <- q2_data |> 
-  mutate(Sample_Date = year(Sample_Date)) |> 
-  filter(Sample_Date >= 1988 & Sample_Date <= 1994) |> 
-  select(Sample_Date, `NH4-N`, Ca, Mg, `NO3-N`, K) |> 
+clean_q2 <- q2_data |>
+  mutate(Year = year(Sample_Date)) |>
+  select(Year, `NH4-N`, Ca, Mg, `NO3-N`, K) |>
+  filter(Year >= 1988 & Year <= 1994) |>
   pivot_longer(
-    cols = !Sample_Date, 
-    names_to = "Ions", 
+    cols = !Year,
+    names_to = "Ions",
     values_to = "Units"
   )
 
+ggplot(
+  clean_q2,
+  mapping = aes(
+    x = Year,
+    y = Units
+  )
+) +
+  geom_point() +
+  facet_wrap(~Ions)
 
 
 # Q3 code ----------------------------------------------------------------
 
-
-clean_q3 <- q3_data |> 
-  mutate(Sample_Date = year(Sample_Date)) |> 
-  filter(Sample_Date >= 1988 & Sample_Date <= 1994) |> 
-  select(Sample_Date, `NH4-N`, Ca, Mg, `NO3-N`, K) |> 
+clean_q3 <- q3_data |>
+  mutate(Year = year(Sample_Date)) |>
+  select(Year, `NH4-N`, Ca, Mg, `NO3-N`, K) |>
+  filter(Year >= 1988 & Year <= 1994) |>
   pivot_longer(
-    cols = !Sample_Date, 
-    names_to = "Ions", 
+    cols = !Year,
+    names_to = "Ions",
     values_to = "Units"
   )
 
 
-
 # MPR code ---------------------------------------------------------------
 
-
-clean_mpr <- mpr_data |> 
-  mutate(Sample_Date = year(Sample_Date)) |> 
-  filter(Sample_Date >= 1988 & Sample_Date <= 1994) |> 
-  select(Sample_Date, `NH4-N`, Ca, Mg, `NO3-N`, K) |> 
+clean_mpr <- mpr_data |>
+  mutate(Year = year(Sample_Date)) |>
+  select(Year, `NH4-N`, Ca, Mg, `NO3-N`, K) |>
+  filter(Year >= 1988 & Year <= 1994) |>
   pivot_longer(
-    cols = !Sample_Date, 
-    names_to = "Ions", 
+    cols = !Year,
+    names_to = "Ions",
     values_to = "Units"
   )
 
@@ -86,3 +146,5 @@ clean_mpr <- mpr_data |>
 # Joining ----------------------------------------------------------------
 
 full_join()
+
+print("hello")
